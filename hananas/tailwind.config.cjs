563422f6cb31216ecf51374fa8b034509db5aad4
Khadja <khadjapaux.contact@gmail.com>
2022-11-09 14:35:18 +0100

    mainpage desing done install axios

--- hananas/tailwind.config.cjs
+++ hananas/tailwind.config.cjs
@@ -3,7 +3,15 @@ module.exports = {
   content: [ "./index.html",
   "./src/**/*.{js,ts,jsx,tsx,html}",],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        'green-blue': '#58A4B0',
+        'soft-dark': '#373F51',
+        'dark': '#1B1B1E',
+        'light-blue': '#A9BCD0',
+        'grey': '#D8DBE2',
+      }
+    },
   },
   plugins: [],
 }
